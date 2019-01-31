c7f52f3c2d347a75aa470d5d991763ff6ff7c092
Fred-Vaniss <46893914+Fred-Vaniss@users.noreply.github.com>
2019-01-31 16:03:55 +0100

    Modal boxes: mise en place des trois premiers boites

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//@ts-check
+
+const log = console.log
+
+var id = document.getElementById.bind(document);
+var clas = document.getElementsByClassName.bind(document);
+var tag = document.getElementsByTagName.bind(document);
+var body = document.getElementsByTagName("body")[0];
+/////////////////////////////////////////////////////
+// Affichage du modal box
+//
+
+function toggleModal(modBoxSel){
+    var modal = id(modBoxSel);
+    var modalContent = document.getElementById(modBoxSel).getElementsByClassName("modal-wrapper")[0];
+
+
+    modal.classList.toggle("show-modal-bg");
+    modalContent.classList.toggle("show-modal-content");
+    body.classList.toggle("lock-body");
+
+    window.addEventListener("click", function (event) {
+        if (event.target === modal) {
+            toggleModal(modal.id);
+        }
+    });
+}
